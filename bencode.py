decbe5a0aa8bed49849c4e474141cf14fcb0a3c4
Baris Ikinci <58369631+brsikinci@users.noreply.github.com>
2023-05-27 17:44:41 +0300

    Decoder prototype complete

--- bencode.py
+++ bencode.py
@@ -29,3 +29,28 @@ def decode(data):
             list.append(decode(tokenizedData))
             token = tokenizedData.popleft()
         return list
+
+    elif token == ord("d"):
+        token = tokenizedData.popleft()
+        dict = {}
+        while token != ord("e"):
+            tokenizedData.appendleft(token)
+            key = decode(tokenizedData)
+            dict[key] = decode(tokenizedData)
+            token = tokenizedData.popleft()
+        return dict
+
+    elif chr(token) in "0123456789":
+        strLength = int(chr(token))
+        token = tokenizedData.popleft()
+        if token != ord(":"):
+            raise TypeError("Invalid input")
+        str = ""
+        for i in range(0, strLength):
+            token = tokenizedData.popleft()
+            str += chr(token)
+        if len(str) != strLength:
+            raise TypeError("Invalid input")
+        return str
+
+    raise TypeError("Invalid input")
